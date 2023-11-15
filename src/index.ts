8ae1e53b63a8f8adac502140124e46960eae02cb
Denis Beresnev <shanthi.bunch@gmail.com>
2023-11-16 01:32:37 +0300

    refactor: moved folders from components to src

--- src/index.ts
+++ src/index.ts
@@ -1,4 +1,4 @@
-import Button from './components/common/Button';
-import DesignProvider from './components/provider';
+import Button from './components/Button';
+import DesignProvider from './provider';
 
 export { DesignProvider, Button };
