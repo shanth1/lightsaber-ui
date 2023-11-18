10deb21e8405c944dc5ca39423624f0c3b149fb1
Denis Beresnev <shanthi.bunch@gmail.com>
2023-11-18 14:49:25 +0300

    feat: Added standard props

--- src/common/Box/Box.tsx
+++ src/common/Box/Box.tsx
@@ -1,7 +1,23 @@
-import React from 'react';
+import React, { CSSProperties, FC, ReactNode } from 'react';
 
-const Box = () => {
-  return <div>Box</div>;
+interface IBox {
+  children: ReactNode | string;
+  color?: string;
+  p?: number;
+  px?: number;
+  py?: number;
+}
+
+const Box: FC<IBox> = ({ children, color, p, px, py }) => {
+  const style: CSSProperties = {};
+  style.backgroundColor = color;
+  style.padding = `${p}rem`;
+  style.paddingLeft = `${px}rem`;
+  style.paddingRight = `${px}rem`;
+  style.paddingTop = `${py}rem`;
+  style.paddingBottom = `${py}rem`;
+
+  return <div style={style}>{children}</div>;
 };
 
 export default Box;
