95fc61551c867ae37ca5f717dddc16cf011066bb
Denis Beresnev <shanthi.bunch@gmail.com>
2023-11-18 20:54:34 +0300

    refactor: updated prettier configuration

--- src/common/Button/Button.tsx
+++ src/common/Button/Button.tsx
@@ -1,14 +1,14 @@
-import React, { FC, ReactNode, useContext } from 'react';
-import './Button.css';
-import { DesignContext, IDesignConfig } from '../../provider';
-import { defaultConfig } from '../../provider/data/defaultConfig';
+import React, { FC, ReactNode, useContext } from "react";
+import "./Button.css";
+import { DesignContext, IDesignConfig } from "../../provider";
+import { defaultConfig } from "../../provider/data/defaultConfig";
 
 interface IButton {
-  children: ReactNode | string;
+    children: ReactNode | string;
 }
 
 export const Button: FC<IButton> = ({ children, ...props }) => {
-  const config: IDesignConfig = useContext(DesignContext) || defaultConfig;
+    const config: IDesignConfig = useContext(DesignContext) || defaultConfig;
 
-  return <button {...props}>{children}</button>;
+    return <button {...props}>{children}</button>;
 };
