69295e1ca14a7d1609380be0ea4553653e3af7f3
Denis Beresnev <shanthi.bunch@gmail.com>
2023-11-18 13:48:17 +0300

    feat: added mixedConfig
    new config complements the default one and does not overwrite it

--- src/common/Button/Button.tsx
+++ src/common/Button/Button.tsx
@@ -1,13 +1,14 @@
 import React, { FC, ReactNode, useContext } from 'react';
 import './Button.css';
 import { DesignContext, IDesignConfig } from '../../provider';
+import { defaultConfig } from '../../provider/data/defaultConfig';
 
 interface IButton {
   children: ReactNode | string;
 }
 
 export const Button: FC<IButton> = ({ children, ...props }) => {
-  const config: IDesignConfig = useContext(DesignContext);
+  const config: IDesignConfig = useContext(DesignContext) || defaultConfig;
 
   return <button {...props}>{children}</button>;
 };
