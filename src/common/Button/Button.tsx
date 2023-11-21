6cfbde9ac995b2f74ca195a06db1f28ce2b2439a
Denis Beresnev <shanthi.bunch@gmail.com>
2023-11-21 23:51:58 +0300

    feat: button from div and styles

--- src/common/Button/Button.tsx
+++ src/common/Button/Button.tsx
@@ -1,11 +1,38 @@
-import React, { FC, ReactNode, useContext } from "react";
+import React, { CSSProperties, FC, ReactNode, useContext } from "react";
 import "./Button.css";
 import { DesignContext, IDesignConfig } from "../../provider";
 import { defaultConfig } from "../../provider/data/defaultConfig";
 import { IProps } from "../../types";
+import { getCustomClassName } from "../../styles/borderRadius";
+import { getStyleObjFromCss } from "../../utils/cssToObj";
+import { updatePadding } from "../../styles/padding";
 
-export const Button: FC<IProps> = ({ children, ...props }) => {
+interface IButton extends IProps {
+    borderRadius?: number;
+}
+
+export const Button: FC<IButton> = (props) => {
     const config: IDesignConfig = useContext(DesignContext) || defaultConfig;
 
-    return <button {...props}>{children}</button>;
+    const customClassName = getCustomClassName(
+        config.borderRadius,
+        props.className
+    );
+
+    const styleFromCss: CSSProperties = getStyleObjFromCss(props.css);
+    const customStyle: CSSProperties = { ...styleFromCss, ...props.style };
+
+    customStyle.backgroundColor = props.color || customStyle.backgroundColor;
+    customStyle.borderRadius = `${props.borderRadius}rem`;
+    updatePadding(customStyle, props.p, props.px, props.py);
+
+    return (
+        <div
+            role="button"
+            className={["button", customClassName].join(" ")}
+            style={customStyle}
+        >
+            {props.children}
+        </div>
+    );
 };
