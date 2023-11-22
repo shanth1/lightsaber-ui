25ae163fefd4c7158e0f301896707e24c8d67efc
RuslanApkin <ruslan-apkin@yandex.ru>
2023-11-22 22:07:13 +0500

    feat:
    copied everithing from box.tsx :)

--- src/common/Modal/Modal.tsx
+++ src/common/Modal/Modal.tsx
@@ -1,15 +1,31 @@
-import React, { FC, ReactNode } from 'react'
+import React, { CSSProperties, FC, useContext } from 'react'
+import { getStyleObjFromCss } from "../../utils/cssToObj";
+import { IProps } from "../../types";
+import { updatePadding } from "../../styles/padding";
+import { DesignContext, IDesignConfig } from "../../provider";
+import { defaultConfig } from "../../provider/data/defaultConfig";
+import "../../styles/border.css";
+import { getCustomClassName } from "../../styles/borderRadius";
 
-interface IModal {
-    children: ReactNode | string;
-    color?: string;
-  }
-  
-  const Modal: FC<IModal> = ({ children, color, ...props }) => {
+interface IModal extends IProps {
+  borderRadius?: number;
+}
+  const Modal: FC<IModal> = ({ children, color, borderRadius, p, px, py, className, style, css  }) => {
+    const config: IDesignConfig = useContext(DesignContext) || defaultConfig;
+
+    const customClassName = getCustomClassName(config.borderRadius, className);
+
+    const styleFromCss = getStyleObjFromCss(css);
+    const customStyle: CSSProperties = { ...styleFromCss, ...style };
+
+    customStyle.backgroundColor = color || customStyle.backgroundColor;
+    customStyle.borderRadius = `${borderRadius}rem`;
+    updatePadding(customStyle, p, px, py);
+
     return (
-      <div {...props} style={{ color }}>
-        {children}
-      </div>
+        <div className={customClassName} style={customStyle}>
+            {children}
+        </div>
     );
   };
 
