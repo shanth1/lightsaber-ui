def4ea8c05bacdbf6d2d4d59b84a241d5629f71e
Denis Beresnev <shanthi.bunch@gmail.com>
2023-11-23 22:42:34 +0300

    feat: Added text align props

--- src/common/Typography/Typography.tsx
+++ src/common/Typography/Typography.tsx
@@ -2,24 +2,32 @@ import React, { CSSProperties, FC, useContext } from "react";
 import { DesignContext, IDesignConfig } from "../../provider";
 import { defaultConfig } from "../../provider/data/defaultConfig";
 import { getStyleObjFromCss } from "../../utils/cssToObj";
-import { getCustomClassName } from "../../styles/borderRadius";
 
 interface ITypography {
     children: string;
     className?: string;
     css?: string;
     style?: CSSProperties;
+
+    start?: boolean;
+    left?: boolean;
+    center?: boolean;
+    end?: boolean;
+    right?: boolean;
+    justify?: boolean;
 }
 
 export const Typography: FC<ITypography> = (props) => {
     const config: IDesignConfig = useContext(DesignContext) || defaultConfig;
-    const customClassName = getCustomClassName(
-        config.borderRadius,
-        props.className
-    );
+    const customClassName = props.className;
 
     const styleFromCss = getStyleObjFromCss(props.css);
     const customStyle: CSSProperties = { ...styleFromCss, ...props.style };
+
+    if (props.justify) customStyle.textAlign = "justify";
+    if (props.end || props.right) customStyle.textAlign = "right";
+    if (props.center) customStyle.textAlign = "center";
+    if (props.start || props.left) customStyle.textAlign = "left";
 
     return (
         <div className={customClassName} style={customStyle}>
