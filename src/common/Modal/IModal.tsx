a4219145cac6603e5e17545c98875d6822f20ae2
RuslanApkin <ruslan-apkin@yandex.ru>
2023-11-27 18:19:15 +0500

    feat: simpe modal done
    close on clicking outside
    scroll disabled when open

--- src/common/Modal/IModal.tsx
+++ src/common/Modal/IModal.tsx
@@ -3,7 +3,8 @@ import { IProps } from "../../types";
 type CallbackFunction = () => void;
 
 export interface IModal extends IProps {
-  borderRadius?: number;
-  isOpen: boolean;
-  onClose: CallbackFunction;
+    borderRadius?: number;
+    isOpen: boolean;
+    onClose: CallbackFunction;
+    bpColor: string; //Back plate color
 }
--- src/common/Modal/Modal.tsx
+++ src/common/Modal/Modal.tsx
@@ -1,21 +1,26 @@
-import React, { CSSProperties, FC, useContext } from 'react'
+import React, { CSSProperties, FC, useContext, useEffect } from "react";
 import { getStyleObjFromCss } from "../../utils/cssToObj";
 import { updatePadding } from "../../styles/padding";
 import { DesignContext, IDesignConfig } from "../../provider";
 import { defaultConfig } from "../../provider/data/defaultConfig";
 import "../../styles/border.css";
 import { getCustomClassName } from "../../styles/borderRadius";
-import "./Modal.css"
-import { IModal } from './IModal';
+import "./Modal.css";
+import { IModal } from "./IModal";
 
-export const Modal: FC<IModal> = ({ 
-  children, 
-  color, 
-  borderRadius, 
-  p, px, py, 
-  className, 
-  style, css, 
-  isOpen, onClose  
+export const Modal: FC<IModal> = ({
+    children,
+    color,
+    borderRadius,
+    p,
+    px,
+    py,
+    className,
+    style,
+    css,
+    isOpen,
+    onClose,
+    bpColor, //? back plate color. not sure about the name yet
 }) => {
     const config: IDesignConfig = useContext(DesignContext) || defaultConfig;
 
@@ -28,11 +33,22 @@ export const Modal: FC<IModal> = ({
     customStyle.borderRadius = `${borderRadius}rem`;
     updatePadding(customStyle, p, px, py);
 
+    useEffect(()=>{
+      document.body.style.overflow = isOpen ? "hidden" : "visible";
+    }, [isOpen])
+
     return isOpen ? (
-      <div className=''>
-        <div className={customClassName} style={customStyle}>
-            {children}
+        <div className="back-plate" style={bpColor ? {backgroundColor: bpColor} : {}} onClick={onClose}>
+            <div className="modal-wrapper">
+                <div
+                    className={"modal-window " + customClassName}
+                    style={customStyle}
+                >
+                    {children}
+                </div>
+            </div>
         </div>
-      </div>
-    ) : <></>;
+    ) : (
+        <></>
+    );
 };
