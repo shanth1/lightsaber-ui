2ec75253acc9084a7241583e9249bbf7cf08382a
Denis Beresnev <shanthi.bunch@gmail.com>
2023-12-05 20:59:50 +0300

    fix: simplified and fix modal logic

--- src/common/Modal/Modal.tsx
+++ src/common/Modal/Modal.tsx
@@ -1,39 +1,27 @@
-import React, { FC, useEffect } from "react";
+import React, { FC } from "react";
 import { IModalProps } from "./types";
-import { getPrimaryStyles } from "../../styles";
 import "./Modal.css";
 
 export const Modal: FC<IModalProps> = (props) => {
-    const style = getPrimaryStyles(props);
+    const style = { background: props.color };
 
-    const CloseBtn = props.closeBtn ? props.closeBtn : <div>X</div>; // For now 'X' is default close btn
-
-    useEffect(() => {
-        document.body.style.overflow = props.isOpen ? "hidden" : "visible";
-    }, [props.isOpen]);
-
-    return props.isOpen ? (
-        <div className="lui-modal-wrapper">
-            <div className="lui-modal-content-wrapper">
+    return (
+        props.isOpen && (
+            <div className="lui-modal" onClick={props.onClose}>
                 <div
-                    className="lui-modal-background-plate"
-                    style={
-                        props.bpColor ? { backgroundColor: props.bpColor } : {}
-                    }
-                    onClick={props.onClose}
-                />
-                <div
-                    className={"lui-modal-cotent " + props.className}
+                    className="lui-modal-content"
                     style={style}
+                    onClick={(e) => e.stopPropagation()}
                 >
                     {props.children}
-                </div>
-                <div onClick={props.onClose} className="lui-modal-close-btn">
-                    {CloseBtn}
+                    {props.showCloseButton && (
+                        <div
+                            onClick={props.onClose}
+                            className="lui-close-button "
+                        ></div>
+                    )}
                 </div>
             </div>
-        </div>
-    ) : (
-        <></>
+        )
     );
 };
