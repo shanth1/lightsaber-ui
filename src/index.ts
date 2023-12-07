84c71e887007ba451f86d6418fa71c40dc9afa4a
Denis Beresnev <shanthi.bunch@gmail.com>
2023-12-07 21:11:20 +0300

    refactor: Moved modal to ref status

--- src/index.ts
+++ src/index.ts
@@ -2,8 +2,6 @@ import { Typography, Text } from "./common/Typography";
 import Button from "./common/Button";
 import Box from "./common/Box";
 import DesignProvider from "./provider";
-import Divider from './common/Divider';
-import Modal from './common/Modal';
+import Divider from "./common/Divider";
 
-export { DesignProvider, Button, Box, Typography, Text, Divider, Modal };
-
+export { DesignProvider, Button, Box, Typography, Text, Divider };
